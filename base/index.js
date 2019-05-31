20e9b85e5f0ed9c2c81d96bfc9b13377271485ea
xiaofan <1353815633@qq.com>
2019-05-31 22:40:20 +0800

    base01

---
+++ base/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: DevZhang 
+ * @Date: 2019-05-31 22:30:07 
+ * @Last Modified by: DevZhang
+ * @Last Modified time: 2019-05-31 22:37:42
+ */
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        seen: false
+    }
+});
+
+app.message = 'hello xiao ming'
