ada9f46ed9ef5a2ac071366268696d74f0be216c
xiaofan <1353815633@qq.com>
2019-06-01 23:24:33 +0800

    component

--- base/index.js
+++ base/index.js
@@ -2,25 +2,30 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-05-31 22:57:37
+ * @Last Modified time: 2019-06-01 23:46:08
  */
 
+
+
+
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>this is {{todo.text}}</li>'
+})
 
 
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'default text'
-    },
-    methods: {
-        changeMsg: function () {
-            this.message = 'change msg'
-        }
+        items: [
+            {id: 001, text: '001'},
+            {id: 002, text: '002'},
+            {id: 003, text: '003'},
+            {id: 004, text: '004'},
+        ]
     }
 })
-
-
-
 
 
 
