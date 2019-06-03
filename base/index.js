870cd70dfa08c9bb967e054365a78ba885961279
xiaofan <1353815633@qq.com>
2019-06-03 17:13:56 +0800

    初步接触 Vue 声明周期函数

--- base/index.js
+++ base/index.js
@@ -2,30 +2,47 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-01 23:46:08
+ * @Last Modified time: 2019-06-03 13:03:47
  */
 
-
-
-
-
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>this is {{todo.text}}</li>'
-})
 
 
 let app = new Vue({
     el: '#app',
     data: {
-        items: [
-            {id: 001, text: '001'},
-            {id: 002, text: '002'},
-            {id: 003, text: '003'},
-            {id: 004, text: '004'},
-        ]
-    }
+        message: 'text'
+    },
+
+    beforeCreate () {
+        console.log('beforeCreate');
+    },
+
+    created() {
+        console.log('created');
+    },
+
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated () {
+        console.log('updated');
+    },
+    beforeDestroy () {
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        console.log('destroyed');
+    },
 })
+
+
+app.message = 'hello';
 
 
 
