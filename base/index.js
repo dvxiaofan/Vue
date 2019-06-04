24d4e162676b1f5515cbd25fc09564b6cb672995
xiaofan <1353815633@qq.com>
2019-06-04 17:11:22 +0800

    计算属性  侦听属性

--- base/index.js
+++ base/index.js
@@ -2,7 +2,7 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-03 22:39:22
+ * @Last Modified time: 2019-06-04 17:10:42
  */
 
 
@@ -10,12 +10,17 @@
 let app = new Vue({
     el: '#app',
     data: {
-        message: 'text'
+        message: 'text',
+        firstName: 'xiao',
+        lastName: 'fan'
     },
 
     computed: {
         reverseMsg() {
             return this.message.split('').reverse().join('');
+        },
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
         }
     },
 
