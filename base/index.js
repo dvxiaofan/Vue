bf28ebc81532fcbd7640518c9c43f5e770f60050
xiaofan <1353815633@qq.com>
2019-06-04 17:44:04 +0800

    watch 侦听属性

--- base/index.js
+++ base/index.js
@@ -2,36 +2,50 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-04 17:10:42
+ * @Last Modified time: 2019-06-04 17:41:50
  */
 
 
 
 let app = new Vue({
-    el: '#app',
+    el: '#watch-demo',
     data: {
-        message: 'text',
-        firstName: 'xiao',
-        lastName: 'fan'
+        question: '',
+        answer: 'I cannot five you an answer until you ask a question!'
     },
 
-    computed: {
-        reverseMsg() {
-            return this.message.split('').reverse().join('');
-        },
-        fullName: function() {
-            return this.firstName + ' ' + this.lastName;
+    watch: {
+        question(newQuestion, oldQuestion) {
+            this.answer = 'Waiting for you to stop typing...'
+            this.debounceGetAnswer();
         }
+    },
+
+    created() {
+        this.debounceGetAnswer = _.debounce(this.getAnswer, 500)
     },
 
     methods: {
-        reMessage() {
-            return this.message.split('').reverse().join('');
+        getAnswer() {
+            if (this.question.indexOf('?') === -1) {
+                this.answer = 'Question usually contain a question mark.'
+                return
+            }
+            this.answer = 'Thinking...'
+            let vm = this;
+            axios.get('https://yesno.wtf/api')
+                .then(function(response) {
+                    vm.answer = _.capitalize(response.data.answer)
+                })
+                .catch(function(error) {
+                    vm.answer = 'Error! Could not reach the API. ' + error
+                })
         }
-    },
+    }
+
+    
 })
 
-app.message = 'you'
 
 
 
