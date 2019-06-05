2d354089e0b3596e91f74d46fa879705aadc686e
xiaofan <1353815633@qq.com>
2019-06-05 09:36:06 +0800

    列表渲染

--- base/index.js
+++ base/index.js
@@ -2,50 +2,42 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-04 17:41:50
+ * @Last Modified time: 2019-06-05 09:35:25
  */
 
 
-
-let app = new Vue({
-    el: '#watch-demo',
-    data: {
-        question: '',
-        answer: 'I cannot five you an answer until you ask a question!'
-    },
-
-    watch: {
-        question(newQuestion, oldQuestion) {
-            this.answer = 'Waiting for you to stop typing...'
-            this.debounceGetAnswer();
-        }
-    },
-
-    created() {
-        this.debounceGetAnswer = _.debounce(this.getAnswer, 500)
-    },
-
-    methods: {
-        getAnswer() {
-            if (this.question.indexOf('?') === -1) {
-                this.answer = 'Question usually contain a question mark.'
-                return
-            }
-            this.answer = 'Thinking...'
-            let vm = this;
-            axios.get('https://yesno.wtf/api')
-                .then(function(response) {
-                    vm.answer = _.capitalize(response.data.answer)
-                })
-                .catch(function(error) {
-                    vm.answer = 'Error! Could not reach the API. ' + error
-                })
-        }
-    }
-
-    
+Vue.component('todo-item', {
+    template: '\
+        <li>\
+            {{ title }}\
+            <button v-on:click="$emit(\'remove\')">Remove</button>\
+        </li>\
+        ',
+        props: ['title']
 })
 
+
+new Vue({
+    el: "#todo-demo",
+    data: {
+        newTodoText: '',
+        todos: [
+            { id: 1, title: 'title1' },
+            { id: 2, title: 'title2' },
+            { id: 3, title: 'title3' },
+        ],
+        nextTodoId: 4
+    },
+    methods: {
+        addNewTodo() {
+            this.todos.push({
+                id: this.nextTodoId++,
+                title: this.newTodoText
+            })
+            this.newTodoText = ''
+        }
+    }
+})
 
 
 
