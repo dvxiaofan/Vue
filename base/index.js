103ef5160f18b76db4ea511461737ccf919a6e95
xiaofan <1353815633@qq.com>
2019-06-27 13:06:38 +0800

    slot 01

--- base/index.js
+++ base/index.js
@@ -2,34 +2,23 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-05 16:29:14
+ * @Last Modified time: 2019-06-27 13:03:58
  */
 
 
-
-Vue.component('my-component', {
-    template: '#my-component'
+Vue.component('bodyContent', {
+    props: ['content'],
+    template: `
+        <div>
+            <slot name='header'></slot>
+            <div class='content'>content</div>
+            <slot name='footer'></slot>
+        </div>`
 })
 
-
-new Vue({
-    el: "#demo",
-    data: {
-        showDiv: false
-    },
-
-    methods: {
-        getText() {
-            this.showDiv = true;
-            this.$nextTick(function() {
-                let text = document.getElementById('div').innerHTML;
-                console.log('text: ', text);
-            })
-        }
-    }
+let app = new Vue({
+    el: '#app'
 })
-
-
 
 
 
