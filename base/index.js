9c0fbdad74c6cf092b3d008c9382c3377a9f4232
xiaofan <1353815633@qq.com>
2019-06-27 13:12:49 +0800

    作用域 slot

--- base/index.js
+++ base/index.js
@@ -2,17 +2,24 @@
  * @Author: DevZhang 
  * @Date: 2019-05-31 22:30:07 
  * @Last Modified by: DevZhang
- * @Last Modified time: 2019-06-27 13:03:58
+ * @Last Modified time: 2019-06-27 13:11:59
  */
 
 
 Vue.component('bodyContent', {
     props: ['content'],
+    data: () => {
+        return {
+            list: [1, 3, 5, 9]
+        }
+    },
     template: `
         <div>
-            <slot name='header'></slot>
-            <div class='content'>content</div>
-            <slot name='footer'></slot>
+            <ul>
+                <slot v-for='item of list'
+                    :item='item'>
+                </slot>
+            </ul>
         </div>`
 })
 
